18e573015bccec03f9c999fad05fde43e21fa5cd
jk2pr <jk2praj@gmail.com>
2020-01-31 14:13:17 +0800

    Initial Commit

---
+++ SwiftUI-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Demo
+//
+//  Created by Jitendra Prajapati on 31/01/20.
+//  Copyright © 2020 Jitendra Prajapati. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
