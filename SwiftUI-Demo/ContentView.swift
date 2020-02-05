a17eefd5fe389cd96819c48247440b91c97e3a56
jk2pr <jk2praj@gmail.com>
2020-02-05 17:03:40 +0800

    installed pod and added alamoFire dependency

--- SwiftUI-Demo/ContentView.swift
+++ SwiftUI-Demo/ContentView.swift
@@ -7,18 +7,49 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ContentView: View {
     var body: some View {
         
-        HStack{
-    
-        Text("Hello,")
-            Text("This is Just a simple Demo of HStack")
+       Button(action: {
+            // What to perform
+        self.downloadPDF()
+        })
+       {
+         Text("Download pdf")   // How the button looks like
         }
+        
     }
+    
+    func downloadPDF() -> Void {
+   
+        
+        let url="http://www.africau.edu/images/default/sample.pdf"
+        
+        let destination = DownloadRequest.suggestedDownloadDestination(for: .documentDirectory)
+
+        Alamofire.download(
+            url,
+            method: .get,
+            encoding: JSONEncoding.default,
+            headers: nil,
+            to: destination).downloadProgress(closure: { (progress) in
+                //progress closure
+            }).response(completionHandler: { (DefaultDownloadResponse) in
+                
+                let response=DefaultDownloadResponse.response.debugDescription
+                print("Response is " + (response as! String))
+                //here you able to access the DefaultDownloadResponse
+                //result closure
+            })
+}
 }
 
+protocol DownloadCallBack
+{
+    
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
